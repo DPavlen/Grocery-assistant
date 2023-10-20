3d5955908f393c876e93d2410fc8fbb6935e7937
Pavlen <pavlen@192.168.1.70>
2023-10-20 17:05:08 +0300

    Edit Actions Secrets

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -121,9 +121,6 @@ DJOSER = {
     "HIDE_USERS": False,
 }
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
@@ -134,9 +131,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
