2ae090773fab696417146ddec684590b65038aa9
Pavlen <pavlen@192.168.1.80>
2023-10-21 15:25:23 +0300

    edit ngnix.conf

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -130,18 +130,18 @@ USE_L10N = True
 USE_TZ = True
 
 
-# STATIC_URL = "/static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_URL = '/backend_static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_URL = '/backend_static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# MEDIA_URL = "/media/"
-# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-MEDIA_URL = '/backend_media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/backend_media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGGING = {
     "version": 1,
