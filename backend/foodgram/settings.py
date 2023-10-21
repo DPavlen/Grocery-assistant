b9d4c515b9e00e61d3e63b180bf2a4f5be332219
Pavlen <pavlen@192.168.1.80>
2023-10-21 12:16:15 +0300

    .yml certbot

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -67,7 +67,7 @@ WSGI_APPLICATION = "foodgram.wsgi.application"
 
 DATABASES = {
     "default": {
-        # Меняем настройку Django: теперь для работы будет использов
+        # Меняем настройку Django: теперь для работы будет использо
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("POSTGRES_DB", "django"),
         "USER": os.getenv("POSTGRES_USER", "django"),
